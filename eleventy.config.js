cc4763587879e06e9bf4f0502728428755a31d1a
DallasArmieri <dparmieri@gmail.com>
2025-12-08 21:59:37 +0000

    path prefix

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,11 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/projects");
 
-  return {
-    dir: {
-      input: "src",
-      output: "_site"
-    }
+ return {
+    dir: { input: "src", output: "_site" },
+    pathPrefix: "/Website/" // your repo name
   };
-   pathPrefix: "/Website/"
 };
