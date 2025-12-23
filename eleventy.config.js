2eae1d5aa62cc281d9e946ec9927d77b1e763907
DallasArmieri <dparmieri@gmail.com>
2025-12-23 18:11:53 +0000

    dropdown add

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/projects");
+  eleventyConfig.addPassthroughCopy("src/images");
 
  return {
     dir: { input: "src", output: "_site" },
